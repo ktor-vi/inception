aba62eac1b8010839f958c8a2749014989dce586
Victor Philippe <me@ktorvi.me>
2025-02-25 15:24:23 +0100

    LGTM

--- srcs/requirements/wordpress/tools/entrypoint.sh
+++ srcs/requirements/wordpress/tools/entrypoint.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 # Si le dossier /var/www/html est vide, alors on copie WordPress
-if [ ! "$(ls -A /var/www/html)" ]; then
-    echo "📂 /var/www/html is empty, copying WordPress..."
+    rm -rf /var/www/html/*
     cp -R /tmp/wordpress/* /var/www/html/
     chown -R www-data:www-data /var/www/html
-else
-    echo "✅ is installed"
-fi
+    chmod -R 755 /var/www/html
 
+echo "<?php phpinfo(); ?>" > /var/www/html/info.php
 # Démarrer PHP-FPM
 exec php-fpm7.4 -F
